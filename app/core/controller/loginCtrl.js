a6f0a58056e7d7e27ce9f7c42b08eb39f62b0c05
Rubens Batista <rubens.sousa@ccc.ufcg.edu.br>
2017-08-15 23:00:40 -0300

    alteracoes no controller do login

--- app/core/controller/loginCtrl.js
+++ app/core/controller/loginCtrl.js
@@ -1,3 +1,49 @@
-app.controller("loginCtrl", function ($scope){
+app.controller("loginCtrl", function ($scope,$http){
+
+	$scope.userLogado;
+
+	//por enquanto
+	$scope.autenticar = function(login,senha){
+		$scope.userLogado = {};
+		console.log($scope.userLogado);
+		alert("Adm logado!");
+	};
+
+	$scope.deslogar = function(){
+		$scope.userLogado = undefined;
+	};
+
+	$scope.isLogado = function(){
+		return !!$scope.userLogado;
+	};
+
+	/*
+	$scope.autenticar = function(login,senha){
+		var adms = $scope.searchAveragePerPatient();
+		var encontrado = false;
+		for (var i = 0; i < adms.length; i++) {
+			adm = adms[i];
+			if(adm.login === login && adm.senha === senha){
+				$scope.userLogado = adm;
+				alert("Administrador logado!");
+				encontrado = true;
+				break;
+			}
+		}
+		if(!encontrado){
+			alert("Administrador não encontrado");
+		}
+
+	}
+	*/
+
+	$scope.searchAveragePerPatient = function () {
+        $http.get("http://localhost:5000/SpringBootRestApi/administradores").then(function successCallback(response) {
+            return response.data.obj;
+        }, function errorCallback(error) {
+            console.log("Unidade Não Encontrada");
+        });
+    }
+
 
 });
